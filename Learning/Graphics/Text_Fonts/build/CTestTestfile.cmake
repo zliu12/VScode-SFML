5d49125fee89c614e70b22378739148541f3230f
QiChang Liu <qcl@QiChangdeAir.attlocal.net>
2021-03-05 13:59:56 -0800

    update

---
+++ Learning/Graphics/Text_Fonts/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/qcl/pcc/spring2021/SFML/Learning/Graphics/Text_Fonts
+# Build directory: /Users/qcl/pcc/spring2021/SFML/Learning/Graphics/Text_Fonts/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- Learning/Window/Open_Manage_Sfml_Window/main.cpp
+++ Learning/Window/Open_Manage_Sfml_Window/main.cpp
@@ -2,21 +2,42 @@
 #include <iostream>
 
 int main() {
-  // sf::Window window(sf::VideoMode(600, 800), "My window", sf::Style::Default);
-	sf::Window window;
-	window.create(sf::VideoMode(600, 800), "My window", sf::Style::Default);
+	sf::Window window1(sf::VideoMode(600, 800), "My window1", sf::Style::Default);
+	sf::Window window2;
+	window2.create(sf::VideoMode(600, 800), "My window2", sf::Style::Default);
+
+	// Change the position of the window (relatively to the desktop)
+	window1.setPosition(sf::Vector2i(1150, 500));	// Screen center
+	window2.setPosition(sf::Vector2i(0, 1000));		// Screen left bottom corner
+
+	// Change the titile of the window
+	window1.setTitle("This is Window 1");
+	window2.setTitle("This is window 2");
+
+	// Change the size of the window
+	window1.setSize(sf::Vector2u(50, 50));		// 300 x 300
+	window2.setSize(sf::Vector2u(300, 300));	// 1000 x 1000
 
 	// Main/game loop: run the program as long as the window is open
-	while (window.isOpen()) {
+	while (window1.isOpen()) {
 		// Check all the window's events that were triggered since the last
 		// iteration of the loop
-		sf::Event event;
-		while (window.pollEvent(event)) {
+		sf::Event event1;
+		while (window1.pollEvent(event1)) {
 			// "close requested" event: we close the window
-			if (event.type == sf::Event::Closed) {
-				window.close();
+			if (event1.type == sf::Event::Closed) {
+				window1.close();
 			}
 		}
+	}
+	while (window2.isOpen()) {
+		sf::Event event2;
+		while (window2.pollEvent(event2)) {
+			if (event2.type == sf::Event::Closed) {
+				window2.close();
+			}
+		}
+
 	}
 
 	return 0;
