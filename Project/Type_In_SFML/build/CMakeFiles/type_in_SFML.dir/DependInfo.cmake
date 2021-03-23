43649ac96d134e700fc86dc42a35c1ebc708bdae
QiChang Liu <qcl@QiChangdeAir.attlocal.net>
2021-03-22 22:43:47 -0700

    Put update() ~ display() inside the while loop

---
+++ Project/Type_In_SFML/build/CMakeFiles/type_in_SFML.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/qcl/pcc/spring2021/SFML/Project/Type_In_SFML/main.cpp" "/Users/qcl/pcc/spring2021/SFML/Project/Type_In_SFML/build/CMakeFiles/type_in_SFML.dir/main.cpp.o"
+  "/Users/qcl/pcc/spring2021/SFML/Project/Type_In_SFML/typeInSFML.cpp" "/Users/qcl/pcc/spring2021/SFML/Project/Type_In_SFML/build/CMakeFiles/type_in_SFML.dir/typeInSFML.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/homebrew/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Project/Type_In_SFML/build/CMakeFiles/type_in_SFML.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/type_in_SFML.dir/main.cpp.o"
+  "CMakeFiles/type_in_SFML.dir/typeInSFML.cpp.o"
+  "type_in_SFML"
+  "type_in_SFML.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/type_in_SFML.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
