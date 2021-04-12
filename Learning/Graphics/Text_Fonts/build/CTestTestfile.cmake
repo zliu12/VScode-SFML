2b4f95cc1e812386232a584448f62f0365ecdbcb
QiChang Liu <qcl@QiChangdeAir.attlocal.net>
2021-04-12 00:20:18 -0700

    File Learning removed

--- Learning/Graphics/Text_Fonts/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/qcl/pcc/spring2021/SFML/Learning/Graphics/Text_Fonts
-# Build directory: /Users/qcl/pcc/spring2021/SFML/Learning/Graphics/Text_Fonts/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
